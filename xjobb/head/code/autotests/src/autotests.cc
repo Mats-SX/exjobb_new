13b771d9d5524c80dc7b976d422736803919b02f
sx <mats.sxz@gmail.com>
2013-10-30 15:59:35 +0100

    Modified autotests to take threads into account. autotests now measure memory from /proc/pid/status.

--- xjobb/head/code/autotests/src/autotests.cc
+++ xjobb/head/code/autotests/src/autotests.cc
@@ -23,8 +23,8 @@ using namespace std;
 
 void process_mem_usage(
 		const pid_t& pid,
-		double& vm_usage,
-		double& resident_set,
+		unsigned long& vm_usage,
+		unsigned long& resident_set,
 		double& user_time,
 		double& system_time,
 		double& total_time)
@@ -33,45 +33,65 @@ void process_mem_usage(
 	using std::ifstream;
 	using std::string;
 
-	vm_usage	= 0.0;
-	resident_set	= 0.0;
+	vm_usage	= 0;
+	resident_set	= 0;
 	user_time	= 0.0;
 	system_time	= 0.0;
 	total_time	= 0.0;
 
 	// 'file' stat seems to give the most reliable results
 	//
-	stringstream s;
-	s << "/proc/" << pid << "/stat";
+	stringstream s1;
+	s1 << "/proc/" << pid << "/status";
 	
-	ifstream stat_stream(s.str().c_str(), ios_base::in);
+	ifstream status_stream(s1.str().c_str(), ios_base::in);
+
+	stringstream s2;
+	s2 << "/proc/" << pid << "/stat";
+	ifstream stat_stream(s2.str().c_str(), ios_base::in);
 
 	// dummy vars for leading entries in stat that we don't care about
-	//
+	
 	string pid2, comm, state, ppid, pgrp, session, tty_nr;
 	string tpgid, flags, minflt, cminflt, majflt, cmajflt;
-	string cutime, cstime, priority, nice;
-	string O, itrealvalue, starttime;
+	string cutim, cstime, priority, nice, num_threads, itrealvalue;
 
 	// the fields we want
 	//
+	unsigned long starttime;
 	unsigned long vsize;
-	long rss;
+	unsigned long rss;
 	unsigned long utime;
 	unsigned long stime;
 
+	string line;
+	string junk;
+	for (int i = 0; i < 11; ++i)
+		getline(status_stream, line);
+	istringstream iss(line);
+	iss >> junk;
+	iss >> vsize;
+	for (int i = 0; i < 4; ++i)
+		getline(status_stream, line);
+	iss.str(line);
+	iss.clear();
+	iss >> junk;
+	iss >> rss;
+
+	
 	stat_stream >> pid2 >> comm >> state >> ppid >> pgrp >> session >> tty_nr
 	       	>> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
 		>> utime >> stime >> cutime >> cstime >> priority >> nice
-		>> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+		>> num_threads >> itrealvalue >> starttime; // don't care about the rest
+	
 
 	stat_stream.close();
 
-	long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-	vm_usage	= vsize / 1024.0 / 1024.0;		// mB
-	resident_set	= rss * page_size_kb / 1024.0;		// mB
-	user_time	= (1000.0 * utime) / ((double) sysconf(_SC_CLK_TCK));	// milliseconds
-	system_time	= (1000.0 * stime) / ((double) sysconf(_SC_CLK_TCK));	// milliseconds
+//	long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+	vm_usage	= vsize;		// kB
+	resident_set	= rss;			// kB
+	user_time	= (1000.0 * utime) / (static_cast<double>(sysconf(_SC_CLK_TCK)));	// milliseconds
+	system_time	= (1000.0 * stime) / (static_cast<double>(sysconf(_SC_CLK_TCK)));	// milliseconds
 	total_time	= user_time + system_time;
 }
 
@@ -84,7 +104,7 @@ void process_mem_usage(
  *
  */
 int main(int argc, char** argv) {
-	
+
 	cout << "This is autotesting." << endl;
 
 	ifstream infile(argv[1]);	// Read what to test from file
@@ -100,39 +120,50 @@ int main(int argc, char** argv) {
 	// causes really strange errors....
 	string arg; 
 	
+	string protocol_name(argv[1]);
+	int pos = protocol_name.rfind("/");
+	protocol_name = protocol_name.substr(pos + 1);
+
 	string prg;
 	iss >> prg;
 
-	int rows;
-	iss >> rows;
-
-	stringstream ss;
-	ss << prg << "_test_res";
+	ostringstream ss;
+	ss << "output/" << prg.substr(5) << "_" << protocol_name << "_res";
 	ofstream out(ss.str().c_str());
 
-	for (int i = 0; i < rows; ++i) {
+	while (true) {
 		do {
 			getline(infile, line);
 		} while (line[0] == '#');
 		
+		// end of this test
+		if (line == "@@@") {
+			out << "@@@" << endl;
+			continue;
+		}
+
+		// end of test protocol file
+		if (line == "@@@@") {
+			break;
+		}
+
 		iss.str(line);
 		iss.seekg(0);
 
-		int k;
-		iss >> k;
-	
 		int nbr_of_tests;
 		iss >> nbr_of_tests;
+
+		int threads;
+		iss >> threads;
 
 		for (int l = 0; l < nbr_of_tests; ++l) {
 			iss >> arg;
 			
-			out << arg << " " << k << endl;
+			out << arg << " " << threads << endl;
 			
 			stringstream cmd;
 			cmd 	<< prg << " input/"
-				<< arg << " "
-				<< k; 
+				<< arg << " " << threads;
 
 			cout << "Now running: " << cmd.str() << endl;
 
@@ -164,20 +195,26 @@ int main(int argc, char** argv) {
 			waitpid(pid, NULL, WUNTRACED);
 			
 			/* Measure child performance */
-			double vm, rss, ut, st, tt;
+			double ut, st, tt;
+			unsigned long vm, rss;
 			process_mem_usage(pid, vm, rss, ut, st, tt);
 			
+			unsigned long _ut = static_cast<unsigned long>(ut);
+			unsigned long _tt = static_cast<unsigned long>(tt);
+
 			/* Print stats to stdout */
-			cout	<< "User time used:\t\t" << (ut) << " ms" 	<< endl
+			cout	<< "User time used:\t\t" << (_ut) << " ms" 	<< endl
 				<< "System time used:\t" << (st) << " ms"	<< endl
-				<< "Total time used:\t" << (tt) << " ms"	<< endl
-				<< "Virtual Memory:\t\t" << vm << " MB"		<< endl
-				<< "Resident set size:\t" << rss << " MB"	<< endl;
+				<< "Total time used:\t" << (_tt) << " ms"	<< endl
+				<< "Virtual Memory:\t\t" << vm << " KB"		<< endl
+				<< "Resident set size:\t" << rss << " KB"	<< endl;
 			
 			/* Print stats to outfile */
-			out	<< "ut, st, tt: " << ut << " " << st << " " << tt << endl;
+			out	<< "ut, st, tt: " << (_ut) << " " << st << " " << (_tt) << endl;
 			out	<< "vm, rss: " << vm << " " << rss << endl;
 
+			/* Let child terminate (to release memory) */
+			kill(pid, SIGCONT);
 
 			/* Let's proceed to next test. */
 			
@@ -187,9 +224,9 @@ int main(int argc, char** argv) {
 	out.flush();
 	out.close();
 
-	cout << "Output written to output/" << ss.str() << endl;
+	cout << "Output written to " << ss.str() << endl;
 
-	execl("/usr/bin/mv", "/usr/bin/mv", ss.str().c_str(), "output", NULL);
+	//execl("/usr/bin/mv", "/usr/bin/mv", ss.str().c_str(), "output", NULL);
 
 	return 0;
 }
