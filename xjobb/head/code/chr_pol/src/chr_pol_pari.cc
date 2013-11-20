7a62680aa480ddd83562aa18b281f7716a36cfd7
sx <mats.sxz@gmail.com>
2013-11-20 14:09:39 +0100

    Debug is no longer default compilation mode. The program now takes another argument for evaluation of only one point. Basically stopped supporting the NTL implementation from now on.

--- xjobb/head/code/chr_pol/src/chr_pol_pari.cc
+++ xjobb/head/code/chr_pol/src/chr_pol_pari.cc
@@ -9,8 +9,11 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-	if (argc != 3)
-		cout << "Usage: <prg> <infile> <threads>" << endl;
+	u_int_t q = -1;
+	if (argc == 4) {
+		q = atoi(argv[3]);
+	} else if (argc != 3)
+		cout << "Usage: <prg> <infile> <threads> [q]" << endl;
 
 	ifstream infile(argv[1]);
 	
@@ -26,28 +29,33 @@ int main(int argc, char** argv) {
 	utils::parse(infile, &matrix, n);
 
 	u_int_t stack_size = PARI_STACK_CHUNK * n;
-	
-	pari_init(stack_size, 0);
-	
-	/* Evaluate the chromatic polynomial in n+1 points */
 
-	string** points = new string*[n + 1];
-	for (u_int_t i = U_ZERO; i - 1 != n; ++i) {
-		cout << "Evaluating x(" << i << ")... " << flush;
-		points[i] = utils::count_colourings_small_space(n, m, i, &matrix, threads);
-		cout << " = " << *points[i] << endl;
+	pari_init(stack_size, 0);
+
+	if (q == -1) {
+		/* Evaluate the chromatic polynomial in n+1 points */
+
+		string** points = new string*[n + 1];
+		for (u_int_t i = U_ZERO; i - 1 != n; ++i) {
+			cout << "Evaluating x(" << i << ")... " << flush;
+			points[i] = utils::count_colourings_small_space(n, m, i, &matrix, threads);
+			cout << " = " << *points[i] << endl;
+		}
+
+		/* Interpolate the evaluated points */
+		interface::print_interpolate(points, n);
+
+		delete[] points;
+	} else {
+		/* Evaluate only at one point */
+		/* This is for the decision problem */
+
+		string* s = utils::count_colourings_small_space(n, m, q, &matrix, threads);
+		cout << *s << endl;
 	}
 	delete[] matrix;
 
-	/* Interpolate the evaluated points */
-	interface::print_interpolate(points, n);
 
 	pari_close();
-	delete[] points;
 
-	
-//	cout << "Now pausing for collection of statistics. PID is " << getpid() << endl;
-//	cout << "Kill me with 'kill -s 9 <PID>' if necessary." << endl;
-//	raise(SIGSTOP);
-	
 }
